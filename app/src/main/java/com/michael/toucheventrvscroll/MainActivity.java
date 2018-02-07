2a5e727765d9e81dc108788d283ea61c9d06d01f
HuangKunTing <382968311@qq.com>
2018-02-07 16:18:48 +0800

    第一次提交

---
+++ app/src/main/java/com/michael/toucheventrvscroll/MainActivity.java
@@ -0,0 +1,19 @@
+package com.michael.toucheventrvscroll;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.show_detial, new RecyclerViewFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
